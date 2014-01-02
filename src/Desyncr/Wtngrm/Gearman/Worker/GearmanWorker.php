ba7104cfbf7dbd24a13691f52d7c34deb0d43c03
DC* <dc@syncr.com.ar>
2014-01-02 12:44:25 -0300

    GearmanWorker extends AbstractWorker

--- src/Desyncr/Wtngrm/Gearman/Worker/GearmanWorker.php
+++ src/Desyncr/Wtngrm/Gearman/Worker/GearmanWorker.php
@@ -1,7 +1,14 @@
 <?php
 namespace Desyncr\Wtngrm\Gearman\Worker;
 
-class GearmanWorker
-{
+use Desyncr\Wtngrm\Worker\AbstractWorker;
 
+abstract class GearmanWorker extends AbstractWorker
+{
+    protected $workload = null;
+
+    public function setUp($sm, $job) {
+        parent::setUp($sm, $job);
+        $this->workload = json_decode($this->job->workload(), true);
+    }
 }
